b1316ed4d45689ed2e044d649839bc14922db317
PabVM <pandvilmas@gmail.com>
2023-01-11 12:03:30 -0300

    avanzando en la implementacion del algoritmo MTA

--- analisis/mta.py
+++ analisis/mta.py
@@ -1,7 +1,6 @@
 import numpy as np
 import json
 
-
 """
 Primero tenemos que crear la traza de los mensajes
 """
@@ -29,3 +28,35 @@ por cada vez que nos encontremos con un 1 en la traza de perdida
 """
 
 loss_lengths = []
+
+"""
+Se recorre el arreglo correspondiente a la traza y cada vez que pille un 1,
+inicializar un contador, dejar que corra hasta encontrar un cero y luego hacerle append a loss_lenghts
+"""
+def count_loss(trc,index):
+    i = index
+    count = 0
+    while trc[i] == 1:
+        count += 1
+        i += 1
+    loss_lengths.append(count)
+    return i
+
+ind = 0
+while ind < trace.size:
+    if trace[ind] == 1:
+        ind = count_loss(trace,ind)
+    else: ind +=1
+
+mean = np.mean(loss_lengths)
+std = np.std(loss_lengths)
+
+print(f'Loss lengths mean: {mean}')
+print(f'Standard deviation: {std}')
+
+c = mean + std
+
+print(f'Change-of-state constant C: {c}')
+
+
+
